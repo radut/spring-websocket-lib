d16d275ce60fd314bad924b57bfd5084f11bf055
Radu Toader <tr@everymatrix.com>
2016-02-01 18:11:43 +0200

    annotationsUtils

--- src/main/java/com/omentrack/websocket/config/WebSocketServletDispatcher.java
+++ src/main/java/com/omentrack/websocket/config/WebSocketServletDispatcher.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpSession;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,13 +49,13 @@ public class WebSocketServletDispatcher extends TextWebSocketHandler {
 	
 	@Override
 	public void afterConnectionClosed( WebSocketSession session, CloseStatus status ) throws Exception {
-	
+		
 		removeClient( session );
 	}
 	
 	@Override
 	public void afterConnectionEstablished( WebSocketSession session ) throws Exception {
-	
+		
 		HttpSession httpSession = (HttpSession) session.getAttributes( ).get( "httpSession" );
 		WebSocketClient webSocketClient = new WebSocketClient( session, httpSession );
 		sessions.put( session.getId( ), webSocketClient );
@@ -62,19 +63,19 @@ public class WebSocketServletDispatcher extends TextWebSocketHandler {
 	
 	@Override
 	public void handleTransportError( WebSocketSession session, Throwable exception ) throws Exception {
-	
+		
 		removeClient( session );
 	}
 	
 	@Override
 	protected void handlePongMessage( WebSocketSession session, PongMessage message ) throws Exception {
-	
+		
 		super.handlePongMessage( session, message );
 	}
 	
 	@Override
 	protected void handleTextMessage( WebSocketSession session, TextMessage message ) throws Exception {
-	
+		
 		ObjectMapper objectMapper = jacksonConverter.getObjectMapper( );
 		WebSocketClient webSocketClient = getWebSocketClient( session );
 		
@@ -104,7 +105,7 @@ public class WebSocketServletDispatcher extends TextWebSocketHandler {
 	
 	@Async
 	private void sendMessageInternal( WebSocketClient client, WebSocketResponse message ) {
-	
+		
 		WebSocketSession webSocketSession = client.getWebSocketSession( );
 		try {
 			String messageAsString = jacksonConverter.getObjectMapper( ).writeValueAsString( message );
@@ -117,12 +118,13 @@ public class WebSocketServletDispatcher extends TextWebSocketHandler {
 		} catch ( IOException e ) {
 			logger.warn( "could not send message to client : jSessionId= " + client.getHttpSession( ).getId( )
 									 + " webSocketSessionId="
-									 + webSocketSession.getId( ), e );
+									 + webSocketSession.getId( ),
+					e );
 		}
 	}
 	
 	private WebSocketMessage buildUnSubscribeMessageFor( String subscription, WebSocketClient webSocketClient ) {
-	
+		
 		WebSocketMessage message = new WebSocketMessage( );
 		message.setType( WebSocketMessageType.UNSUBSCRIBE );
 		message.setUrl( subscription );
@@ -130,24 +132,25 @@ public class WebSocketServletDispatcher extends TextWebSocketHandler {
 	}
 	
 	private String combineAndCleanUrl( String wsControllerUrl, String wsMethodUrl ) {
-	
-		return ( wsControllerUrl + "/" + wsMethodUrl ).replaceAll( "//", "/" );
+		
+		return ( wsControllerUrl + "/"
+						 + wsMethodUrl ).replaceAll( "//", "/" );
 	}
 	
 	private WebSocketClient getWebSocketClient( WebSocketSession session ) {
-	
+		
 		return sessions.get( session.getId( ) );
 	}
 	
 	private WebSocketInvocableHandlerMethod getWebSocketInvocableHandlerMethod( WebSocketMessage webSocketMessage ) {
-	
+		
 		Map<String, Object> wsControllers = ctx.getBeansWithAnnotation( WebSocketController.class );
 		
 		WebSocketMessageType type = webSocketMessage.getType( );
 		
 		for ( Entry<String, Object> entry : wsControllers.entrySet( ) ) {
 			Object controller = entry.getValue( );
-			RequestMapping controllerMapping = controller.getClass( ).getAnnotation( RequestMapping.class );
+			RequestMapping controllerMapping = AnnotationUtils.findAnnotation( controller.getClass( ), RequestMapping.class );
 			if ( controllerMapping == null )
 				continue;
 			String[] controllerMappings = controllerMapping.value( );
@@ -158,7 +161,7 @@ public class WebSocketServletDispatcher extends TextWebSocketHandler {
 						switch ( type ) {
 							case GET:
 								if ( method.isAnnotationPresent( WebSocketGet.class ) ) {
-									WebSocketGet get = method.getAnnotation( WebSocketGet.class );
+									WebSocketGet get = AnnotationUtils.findAnnotation( method, WebSocketGet.class );
 									if ( webSocketMessage.getUrl( ).equals( ( combineAndCleanUrl( controllerUrl, get.value( ) ) ) ) ) {
 										return new WebSocketInvocableHandlerMethod( controller, method );
 									}
@@ -166,7 +169,7 @@ public class WebSocketServletDispatcher extends TextWebSocketHandler {
 								break;
 							case SUBSCRIBE:
 								if ( method.isAnnotationPresent( WebSocketSubscribe.class ) ) {
-									WebSocketSubscribe subscribe = method.getAnnotation( WebSocketSubscribe.class );
+									WebSocketSubscribe subscribe = AnnotationUtils.findAnnotation( method, WebSocketSubscribe.class );
 									if ( webSocketMessage.getUrl( ).equals( ( combineAndCleanUrl( controllerUrl, subscribe.value( ) ) ) ) ) {
 										return new WebSocketInvocableHandlerMethod( controller, method );
 									}
@@ -174,7 +177,7 @@ public class WebSocketServletDispatcher extends TextWebSocketHandler {
 								break;
 							case UNSUBSCRIBE:
 								if ( method.isAnnotationPresent( WebSocketUnSubscribe.class ) ) {
-									WebSocketUnSubscribe unSubscribe = method.getAnnotation( WebSocketUnSubscribe.class );
+									WebSocketUnSubscribe unSubscribe = AnnotationUtils.findAnnotation( method, WebSocketUnSubscribe.class );
 									if ( webSocketMessage.getUrl( ).equals( combineAndCleanUrl( controllerUrl, unSubscribe.value( ) ) ) ) {
 										return new WebSocketInvocableHandlerMethod( controller, method );
 									}
@@ -190,12 +193,12 @@ public class WebSocketServletDispatcher extends TextWebSocketHandler {
 	}
 	
 	private Object handleWebSocketMessage( WebSocketClient webSocketClient, ObjectMapper objectMapper, WebSocketMessage webSocketMessage ) throws Exception {
-	
+		
 		synchronized ( webSocketClient.getWebSocketSession( ) ) {
 			WebSocketInvocableHandlerMethod wsihm = getWebSocketInvocableHandlerMethod( webSocketMessage );
 			Object returnValue = wsihm.invokeWithArguments( objectMapper, webSocketMessage.getData( ),//
 					new ReadOnlyWebSocketSession( webSocketClient.getWebSocketSession( ) ), webSocketClient.getHttpSession( ) );
-			
+					
 			switch ( webSocketMessage.getType( ) ) {
 				case SUBSCRIBE:
 					webSocketClient.getSubscriptions( ).add( webSocketMessage.getUrl( ) );
@@ -213,7 +216,7 @@ public class WebSocketServletDispatcher extends TextWebSocketHandler {
 	}
 	
 	private void removeClient( WebSocketSession session ) {
-	
+		
 		WebSocketClient webSocketClient = sessions.remove( session.getId( ) );
 		if ( webSocketClient != null ) {
 			Set<String> subscriptions = webSocketClient.getSubscriptions( );
@@ -229,7 +232,7 @@ public class WebSocketServletDispatcher extends TextWebSocketHandler {
 	}
 	
 	public boolean sendMessageToAllSubscribers( String subscription, Object message ) {
-	
+		
 		List<WebSocketClient> webSocketClientsWithSubscription = new ArrayList<WebSocketClient>( );
 		for ( WebSocketClient webSocketClient : sessions.values( ) ) {
 			if ( webSocketClient.getSubscriptions( ).contains( subscription ) ) {
@@ -242,7 +245,7 @@ public class WebSocketServletDispatcher extends TextWebSocketHandler {
 	}
 	
 	public boolean sendMessageToWebSocketClient( String webSocketSessionId, String subscription, Object message ) {
-	
+		
 		WebSocketClient webSocketClient = sessions.get( webSocketSessionId );
 		if ( webSocketClient != null && webSocketClient.getSubscriptions( ).contains( subscription ) ) {
 			sendMessageToClients( Collections.singletonList( webSocketClient ), subscription, message );
@@ -253,7 +256,7 @@ public class WebSocketServletDispatcher extends TextWebSocketHandler {
 	}
 	
 	public boolean sendMessageToAllHttpSession( String jSessionId, String subscription, Object message ) {
-	
+		
 		List<WebSocketClient> webSocketClientsWithSubscription = new ArrayList<WebSocketClient>( );
 		for ( WebSocketClient webSocketClient : sessions.values( ) ) {
 			if ( webSocketClient.getHttpSession( ).getId( ).equals( jSessionId ) ) {
@@ -268,7 +271,7 @@ public class WebSocketServletDispatcher extends TextWebSocketHandler {
 	
 	@Async
 	private void sendMessageToClients( List<WebSocketClient> clients, String subscription, Object message ) {
-	
+		
 		for ( WebSocketClient wsClient : clients ) {
 			WebSocketResponse webSocketResponse = buildWebSocketResponseMessage( subscription, message );
 			sendMessageInternal( wsClient, webSocketResponse );
@@ -276,7 +279,7 @@ public class WebSocketServletDispatcher extends TextWebSocketHandler {
 	}
 	
 	private WebSocketResponse buildWebSocketResponseMessage( String subscription, Object message ) {
-	
+		
 		WebSocketResponse wsResponse = new WebSocketResponse( );
 		wsResponse.setResultOk( true );
 		wsResponse.setType( WebSocketMessageType.UPDATE );
